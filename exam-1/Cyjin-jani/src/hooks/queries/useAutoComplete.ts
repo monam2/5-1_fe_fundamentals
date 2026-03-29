2d2ddda175ea786d11f5db52b12d6bd56c8d6270
Cyjin-jani <jinjanic91@gmail.com>
2026-03-29 23:54:59 +0900

    docs: 주석 및 readme 등 작성

--- exam-1/Cyjin-jani/src/hooks/queries/useAutoComplete.ts
+++ exam-1/Cyjin-jani/src/hooks/queries/useAutoComplete.ts
@@ -15,10 +15,13 @@ const fetchAutoComplete = async (
   return response.json();
 };
 
+// TODO: 에러 처리 필요..?
 export const useAutoComplete = (keyword: string) => {
   return useQuery<IAutocompleteResponse>({
     queryKey: ['autocomplete', keyword],
     queryFn: () => fetchAutoComplete(keyword),
     enabled: keyword.trim().length > 0,
+    // NOTE: useQueryErrorResetBoundary를 ErrorBoundary와 함께 사용하면 TanStack Query가 에러를 ErrorBoundary로 전파하는 모드로 동작하여 명시적으로 false 처리
+    throwOnError: false,
   });
 };
