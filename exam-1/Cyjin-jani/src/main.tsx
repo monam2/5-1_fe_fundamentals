3d696ab1b931ae0d397ad3f87dd69eb10d5d7206
Cyjin-jani <jinjanic91@gmail.com>
2026-03-29 18:39:55 +0900

    chore: router dom 추가

--- exam-1/Cyjin-jani/src/main.tsx
+++ exam-1/Cyjin-jani/src/main.tsx
@@ -1,6 +1,7 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 
 async function enableMocking() {
@@ -24,9 +25,11 @@ const queryClient = new QueryClient({
 enableMocking().then(() => {
   ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
-      <QueryClientProvider client={queryClient}>
-        <App />
-      </QueryClientProvider>
+      <BrowserRouter>
+        <QueryClientProvider client={queryClient}>
+          <App />
+        </QueryClientProvider>
+      </BrowserRouter>
     </React.StrictMode>,
   );
 });
